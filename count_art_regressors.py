e5da813b56036e8a67b2f9a64df7c8de23b945ff
gardoma <andrea.stocco@gmail.com>
2017-10-12 16:04:29 -0700

    Added support for multiple files in count_art_regressors

--- count_art_regressors.py
+++ count_art_regressors.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
 import scipy.io as io
+import os.path
 import sys
 
 HLP_MSG = """
 Usage:
 
-   count_art_regressors.py <art_file>
+   count_art_regressors.py <art_file1> <art_file2> ... <art_fileN>
 
 Where:
    
-   *  <art_file> is one of the .mat files produced by the
+   *  <art_fileX> is one of the .mat files produced by the
       ART software, which contain various outliers.
 
 The scripts prints out the number of outliers included in
@@ -26,5 +27,8 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print(HLP_MSG)
     else:
-        filename = sys.argv[1]
-        print(count_outliers(filename))
+        for f in sys.argv[1:]:
+            if os.path.isfile(f):
+                print(count_outliers(f))
+            else:
+                raise Exception("No such file: %s" % f)
