0876b625d1e1cb61284c39c7195a989a1fd99249
gardoma <andrea.stocco@gmail.com>
2017-10-12 15:35:29 -0700

    Added new Python script to count ART outliers

---
+++ count_art_regressors.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import scipy.io as io
+import sys
+
+HLP_MSG = """
+Usage:
+
+   count_art_regressors.py <art_file>
+
+Where:
+   
+   *  <art_file> is one of the .mat files produced by the
+      ART software, which contain various outliers.
+
+The scripts prints out the number of outliers included in
+the file.
+
+"""
+
+def count_outliers(artfile):
+    """Counts the number of ourliers in an art file"""
+    D = io.loadmat(artfile)
+    return D["R"].shape[1]
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print HLP_MSG
+    else:
+        filename = sys.argv[1]
+        print(count_outliers(filename))
